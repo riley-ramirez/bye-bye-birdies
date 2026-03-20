1848a2cbd77a2a20bd90ded7d3e10b0ec11eef95
riley-ramirez <rileyramirez@berkeley.edu>
2026-03-20 16:08:39 -0700

    seventh commit

---
+++ src/lib/components/animations.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  /**
+   * Example shortcode usage:
+   * [[CoolNewFeature src="https://example.com/embed" size="large" credit="Name" title="Optional title"]]
+   */
+
+</script>
+
+<body>
+    <h1>This is a project done by Riley.</h1>
+</body>
