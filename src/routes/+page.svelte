892446ca9d9423c3f2303902ae55dc0b9aeaed2a
riley-ramirez <rileyramirez@berkeley.edu>
2026-03-22 20:32:19 -0700

    eigth commith

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,20 +15,23 @@ This code is in +page.svelte and can be manually coded using HTML or Svelte tags
 
   <!-- Video -->
   <video autoplay muted loop playsinline class="bg-video">
-    <source src="videos/0_hero.mp4" type="video/mp4">
+    <source src="videos/0_hero_stablized.mp4" type="video/mp4">
   </video>
 
   <!-- Text content -->
   <div class="hero-content text-white">
-    <h1 class="display-2 hero-title">Bye Bye Birdies</h1>
+    <h1 class="hero-title">
+      <span class="line1">Bye Bye</span><br>
+      <span class="line2">Birdies</span>
+    </h1>
 
-   <p class="fw-light">
+   <p class="fw-light" style="margin-top: 2.5rem; font-size: 1.15rem;">
       Six years after scientists reported that billions of birds vanished from 
       the North American landscape since 1970, conservationists are still racing 
       to reverse catastrophic losses that show no sign of slowing down.
     </p>
 
-    <p class="fw-light" style="font-size: smaller;">
+    <p class="fw-light">
       By Riley Ramirez
     </p>
   </div> 
