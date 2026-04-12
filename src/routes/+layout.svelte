c4a18cf02a9c42123444937152ac868e96035fe8
riley-ramirez <rileyramirez@berkeley.edu>
2026-04-12 01:37:56 -0700

    updates to +layout.svelte for background image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,34 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 	import '../app.scss';
+	import { base } from '$app/paths';
 
 	import { onMount } from 'svelte';
 
 	let { children } = $props();
 
-  onMount(async () => {
-
-    const jqueryModule = await import('jquery');
+	onMount(async () => {
+		const jqueryModule = await import('jquery');
 		window.jQuery = jqueryModule.default;
 		window.$ = window.jQuery;
 
-    // load Bootstrap (bundle includes Popper)
-    await import('bootstrap/dist/js/bootstrap.bundle.min.js');
-  });
+		await import('bootstrap/dist/js/bootstrap.bundle.min.js');
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<div style="background-image: linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url('{base}/images/grid-extended.png')" id="app-background">
+	{@render children()}
+</div>
+
+<style>
+	#app-background {
+		min-height: 100vh;
+		background-size: contain;
+		background-position: center;
+		background-repeat: repeat;
+	}
+</style>
